3a7c2596cf5c9896905eb86fa3fcaa2f68232cd2
Olegs Bugrovs <olegs.bugrovs@pearlgroup.no>
2024-09-02 08:52:29 +0300

    Fixed build script

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -14,7 +14,7 @@ if (!fs.existsSync(typesDir)) fs.mkdirSync(typesDir, {recursive: true})
 
 // use a relative target, in case the user moves the project
 const target = join(typesDir, 'sap__cds')
-const src = resolvePkg('@cap-js/cds-types') ?? join(nodeModules, '@cap-js/cds-types')
+const src = resolvePkg('@goldsucc/cds-types') ?? join(nodeModules, '@goldsucc/cds-types')
 const rel = relative(dirname(target), src) // need dirname or we'd land one level above node_modules (one too many "../")
 console.log(`Creating symlink ${target} -> ${rel}`)
 
@@ -28,6 +28,11 @@ try {
 try {
     // 'junction' is needed to make it work on windows, others ignore
     fs.symlinkSync(rel, target, 'junction')
+    fs.rename(`${src}/dist/cds-types.d.ts`, `${src}/index.d.ts`, function (error) {
+        if (error) {
+            throw error
+        }
+    })
 } catch (e) {
     if (e.code !== 'EEXIST') throw e
     // else: symlink exists (the previous unlink hasn't worked), ignore
